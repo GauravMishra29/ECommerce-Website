1103ee4dcc2920e42874ecce9dc85b9ca4bfd117
gaurav <gauravmishraa2003@gmail.com>
2025-12-02 16:33:33 +0530

    First Commit

---
+++ frontend/src/data/products.js
@@ -0,0 +1,62 @@
+const products = [
+    {
+        id: 1,
+        name: 'smartphone',
+        price: 299.99,
+        description: 'Latest model smartphone with amazing features.',
+        image: '/images/im1.jpg'
+        
+    },
+    {
+        id: 2,
+        name: 'Laptop',
+        price: 799.99,
+        description: 'High-performance laptop for gaming and work.',
+        image: '/images/im2.jpg'
+    },
+    {
+        id: 3,
+        name: 'Headphones',
+        price: 99.99,
+        description: 'Noise-cancelling headphones with premium sound quality.',
+        image: '/images/im3.jpg'
+    },
+    {
+        id: 4,
+        name: 'Headphones',
+        price: 99.99,
+        description: 'Noise-cancelling headphones with premium sound quality.',
+        image: '/images/im4.jpg'
+    },
+    {
+        id: 5,
+        name: 'Headphones',
+        price: 99.99,
+        description: 'Noise-cancelling headphones with premium sound quality.',
+        image: '/images/im2.jpg'
+    },
+    {
+        id: 6,
+        name: 'Headphones',
+        price: 99.99,
+        description: 'Noise-cancelling headphones with premium sound quality.',
+        image: '/images/im2.jpg'
+    },
+    {
+        id: 7,
+        name: 'Headphones',
+        price: 99.99,
+        description: 'Noise-cancelling headphones with premium sound quality.',
+        image: '/images/im2.jpg'
+    },
+    {
+        id: 8,
+        name: 'Headphones',
+        price: 99.99,
+        description: 'Noise-cancelling headphones with premium sound quality.',
+        image: '/images/im2.jpg'
+    },
+    // Add more products here...
+];
+
+export default products;
